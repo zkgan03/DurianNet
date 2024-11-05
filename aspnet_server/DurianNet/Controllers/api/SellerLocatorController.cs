c4fba52cb290042b3eab2e8117dfdea401ab6f60
zhiken <zhiken03@hotmail.com>
2024-11-05 15:14:29 +0800

    setup swagger

---
+++ aspnet_server/DurianNet/Controllers/api/SellerLocatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DurianNet.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerLocatorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
--- aspnet_server/DurianNet/Program.cs
+++ aspnet_server/DurianNet/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,6 +14,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
