d20a8b1e6b6d9c81e425af65eed8f16bedb1380e
TanKaiJian <kaijian883@gmail.com>
2024-11-18 12:58:22 +0800

    done some ui

--- aspnet_server/DurianNet/Controllers/DurianProfileWebController.cs
+++ aspnet_server/DurianNet/Controllers/DurianProfileWebController.cs
@@ -6,6 +6,30 @@ public class DurianProfileWebController : Controller
     [HttpGet("DurianProfilePage")]
     public IActionResult DurianProfilePage()
     {
+        // This loads the main Durian Profile page.
         return View("~/Views/DurianProfile/DurianProfile.cshtml");
+    }
+
+    [HttpGet("AddDurianProfilePage")]
+    public IActionResult AddDurianProfilePage()
+    {
+        return View("~/Views/DurianProfile/AddDurianProfileDetails.cshtml");
+    }
+
+
+    [HttpGet("UpdateDurianProfileDetailsPage")]
+    public IActionResult UpdateDurianProfileDetailsPage(int id)
+    {
+        // TODO: Fetch durian details by ID and pass it to the view for editing.
+        // For now, just load the page.
+        return View("~/Views/DurianProfile/UpdateDurianProfileDetails.cshtml");
+    }
+
+    [HttpGet("Delete")]
+    public IActionResult Delete(int id)
+    {
+        // TODO: Implement delete logic for the durian by ID.
+        // Redirect to the Durian Profile page after deletion.
+        return RedirectToAction("DurianProfilePage");
     }
 }
--- aspnet_server/DurianNet/Controllers/UserAccountWebController.cs
+++ aspnet_server/DurianNet/Controllers/UserAccountWebController.cs
@@ -6,6 +6,31 @@ public class UserAccountWebController : Controller
     [HttpGet("UserAccountPage")]
     public IActionResult UserAccountPage()
     {
+        // Load the main User Account page
         return View("~/Views/UserAccount/UserAccount.cshtml");
+    }
+
+    [HttpGet("ViewUser")]
+    public IActionResult ViewUser(int id)
+    {
+        // TODO: Fetch user details by ID and pass it to the view
+        // For now, just load the page
+        return View("~/Views/UserAccount/UserAccountDetails.cshtml");
+    }
+
+    [HttpGet("DeleteUser")]
+    public IActionResult DeleteUser(int id)
+    {
+        // TODO: Implement logic to delete the user by ID
+        // Redirect to User Account page after deletion
+        return RedirectToAction("UserAccountPage");
+    }
+
+    [HttpGet("RecoverUser")]
+    public IActionResult RecoverUser(int id)
+    {
+        // TODO: Implement logic to recover the user by ID
+        // Redirect to User Account page after recovery
+        return RedirectToAction("UserAccountPage");
     }
 }
